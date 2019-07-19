a73bfc1d5ec838578b540362fe84763c676bbbfa
ugo <ugo@sportr.co.uk>
2019-07-19 14:59:38 +0100

    access places search API

--- localistico/location.py
+++ localistico/location.py
@@ -1,3 +1,9 @@
+import requests
+
+
+APIKEY = 'AIzaSyBDHCARiAh9_LTmreNg74pD_wZF4iyQezU'
+stub_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?{}'
+
 
 class APIError(Exception):
     pass
@@ -10,5 +16,24 @@ class Location:
         self.longitude = longitude
 
 
-def resolve_location(name:str, location:Location):
-    pass
+def resolve_location(name:str, location:Location=None):
+    params = "query={}".format(name)
+    if location:
+        params += "&location={},{}".format(location.latitude, location.longitude)
+    url = stub_url.format(params)
+    resp = requests.get(url)
+    if resp.status_code >= 200 and resp.status_code < 300:
+        rjson = resp.json()
+        if rjson['status'] == "OK":
+            return {
+                'name': rjson['results'][0]['name'],
+                'place_id': rjson['results'][0]['id'],
+                'international_phone_number': rjson['results'][0],
+            }
+        else:
+            if "error_message" in rjson:
+                raise APIError("Error in Google response: {}".format(rjson['error_message']))
+            else:
+                raise APIError(rjson['status'])
+    else:
+        raise APIError("HTTP return code {}".format(resp.status_code))
