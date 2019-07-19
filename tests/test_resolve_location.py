a8ac3ac7e60ab2af2cff8967a032041c7639525e
ugo <ugo@sportr.co.uk>
2019-07-19 17:35:43 +0100

    fixtures to mock google calls

--- tests/test_resolve_location.py
+++ tests/test_resolve_location.py
@@ -1,9 +1,29 @@
 import pytest
+from unittest import mock
 
+from localistico import location
 from localistico.location import resolve_location, Location, APIError
 
 
-def test_resolve_location():
+@pytest.yield_fixture
+def mock_places_search():
+    with mock.patch.object(location, "google_places_search") as _places_search:
+        _places_search.return_value = {'results': [{'place_id': '123'}]}
+        yield _places_search
+
+
+@pytest.fixture
+def mock_places_detail():
+    with mock.patch.object(location, "google_places_detail") as _places_detail:
+        _places_detail.return_value = {'results': [{
+            'name': "Stark Industries",
+            'id': "abc123",
+            'international_phone_number': "+44 1234 567 890",
+        }]}
+        yield _places_detail
+
+
+def test_resolve_location(mock_places_search, mock_places_detail):
     # Arrange
     name = "Stark Industries"
     location = Location(latitude=51.376163, longitude=-0.098234)
@@ -13,13 +33,13 @@ def test_resolve_location():
 
     # Assert
     assert place == {
-        "name": "Stark Industries",
-        "place_id": "abc123",
-        "international_phone_number": "+44 1234 567 890",
+        'name': "Stark Industries",
+        'place_id': "abc123",
+        'international_phone_number': "+44 1234 567 890",
     }
 
 
-def test_resolve_location_error():
+def test_resolve_location_error(mock_places_search, mock_places_detail):
     # Arrange
     name = "Daily Bugle"
     location = Location(latitude=51.012312, longitude=0.082123)
