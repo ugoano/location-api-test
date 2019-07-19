19eafe0ab4594366702f66d685658a316a948fe0
ugo <ugo@sportr.co.uk>
2019-07-19 19:43:10 +0100

    Query based location

--- localisticoapi/gsearch/views.py
+++ localisticoapi/gsearch/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
 
-from localistico.location import resolve_location
+from localistico.location import Location, resolve_location
 
 
 def index(request):
-    resp = resolve_location("My Old Dutch")
+    query = request.GET['query']
+    location_query = None
+    if "latitude" in request.GET and "longitude" in request.GET:
+        location_query = Location(request.GET['latitude'], request.GET['longitude'])
+    resp = resolve_location(name=query, location=location_query)
     return JsonResponse(resp)
