b4ee31e6d8a45c314f5dc552d42c27fd210e4571
ugo <ugo@sportr.co.uk>
2019-07-19 20:39:01 +0100

    Test for query and location

--- tests/test_api.py
+++ tests/test_api.py
@@ -29,3 +29,14 @@ def test_gsearch_valid_character_query():
     resp = requests.get("{}?query={}".format(SERVER_URL, query))
     assert resp.status_code == 200
     assert "error" in resp.json()
+
+
+def test_gsearch_query_and_location():
+    query = "query=Tesco&latitude=51.376163&longitude=-0.098234"
+    resp = requests.get("{}?{}".format(SERVER_URL, query))
+    assert resp.status_code == 200
+    assert resp.json() == {
+        'name': "Tesco Express",
+        'place_id': "2cc0742e2693d3d1ce4774f36d62e352bfcd948d",
+        'international_phone_number': "+44 345 610 2689"
+    }
