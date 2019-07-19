2d9cd93dc4bba8ad287d9bda8e6d5358752fc068
ugo <ugo@sportr.co.uk>
2019-07-19 20:09:20 +0100

    Add validation with serializers

--- localisticoapi/gsearch/views.py
+++ localisticoapi/gsearch/views.py
@@ -1,12 +1,27 @@
 from django.http import JsonResponse
+from rest_framework import serializers, status
 
 from localistico.location import Location, resolve_location
 
 
+class GSearchSerializer(serializers.Serializer):
+    query = serializers.CharField(max_length=20)
+    latitude = serializers.FloatField(required=False)
+    longitude = serializers.FloatField(required=False)
+
+
 def index(request):
+    gsearch_query = GSearchSerializer(data=request.GET)
+    if not gsearch_query.is_valid():
+        return JsonResponse(dict(
+            message=gsearch_query.errors,
+            status=status.HTTP_404_NOT_FOUND,
+        ))
     query = request.GET['query']
     location_query = None
     if "latitude" in request.GET and "longitude" in request.GET:
-        location_query = Location(request.GET['latitude'], request.GET['longitude'])
+        location_query = Location(
+            latitude=request.GET['latitude'],
+            longitude=request.GET['longitude'])
     resp = resolve_location(name=query, location=location_query)
     return JsonResponse(resp)
